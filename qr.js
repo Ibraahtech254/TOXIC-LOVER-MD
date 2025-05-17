c3ca60a4d9aa503b4561f457ecf19037a051ffa6
ibraah tech <munjueko@gmail.com>
2025-05-17 12:50:00 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -8,13 +8,13 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: France_King,
+	default: Maher_Zubair,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
 	delay,
 	makeInMemoryStore,
-} = require("@whiskeysockets/baileys");
+} = require("maher-zubair-baileys");
 
 function removeFile(FilePath) {
 	if (!fs.existsSync(FilePath)) return false;
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function FLASH_MD_QR_CODE() {
+	async function SIGMA_MD_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_France_King = France_King({
+			let Qr_Code_By_Maher_Zubair = Maher_Zubair({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_France_King.ev.on('creds.update', saveCreds)
-			Qr_Code_By_France_Kingr.ev.on("connection.update", async (s) => {
+			Qr_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,33 +56,36 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: 'TOPU;;;' + b64data });
+				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: '' + b64data });
 	
-				   let FLASH_MD_TEXT = `
-       TOXIC-LOVER-MD CONNECTED 
-`
-	 await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
+				   let SIGMA_MD_TEXT = `
+TOXIC-LOVER-MD CONNECTED 
+
+
+Don't Forget To Give Star🌟 To My Repo`
+					
+	 await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_France_King.ws.close();
+					await Qr_Code_By_Maher_Zubair.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					FLASH_MD_QR_CODE();
+					SIGMA_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
 			if (!res.headersSent) {
 				await res.json({
-					code: "Service is Currently Unavailable"
+					code: "Service Unavailable"
 				});
 			}
 			console.log(err);
 			await removeFile("temp/" + id);
 		}
 	}
-	return await FLASH_MD_QR_CODE()
+	return await SIGMA_MD_QR_CODE()
 });
 module.exports = router
